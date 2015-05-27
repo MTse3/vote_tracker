b83f6d71f23e41f477b506935210915fd933bd69
Michael Tse <Michael.Tse3@gmail.com>
2015-05-27 08:51:25 -0700

    created layout for vote_tracker added color scheme and some font styling made so random picture would appear on screens highlights when clicked on

---
+++ README.md
@@ -0,0 +1,4 @@
+This app should have users vote between two kittens shown on the screen and give the total number of votes that they have recieved
+
+
+5-26  worked with marcel to get cat pics on page
---
+++ kitten.js
@@ -0,0 +1,47 @@
+window.onload = function () {
+  var Vote = function() {
+    this.initialize = function() {
+      var $option1 = new ChooseCat();
+      var $option2 = new ChooseCat();
+      $option1.prependTo('#cat-pic');
+      if ($option1 === $option2) {
+        $option2 = new ChooseCat();
+        console.log($option2);
+      } else {
+      $option2.prependTo('#cat-pic2');
+      }
+  }
+  var game = new Vote;
+  Vote.initialize
+
+var catPicsArray = []; //loops through and selects each image
+  for (var i = 1; i <= 14; i++) {
+   catPicsArray.push(i+'.jpg');
+  };
+
+var ChooseCat = function () {
+  return $('<img src="images/' + catPicsArray[Math.floor(Math.random() * catPicsArray.length)] + '">')
+}
+
+
+
+
+//for tracking vote totals
+$('section.grid_6').on('click', function(){
+    var voteTotal = 0;
+    voteTotal+=
+    $('.total').append(voteTotal);
+  })
+
+
+
+$('.vote-again').on('click', function(e) { //click on vote again button
+
+    var $option1 = new ChooseCat();
+    var $option2 = new ChooseCat();
+    e.preventDefault();//avoids refresh after clicking button
+  });
+
+
+
+}
