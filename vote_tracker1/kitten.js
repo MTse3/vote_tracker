f107e63aaa54960baae0be32835a4069222bbdf1
Michael Tse <Michael.Tse3@gmail.com>
2015-05-31 23:31:05 -0700

    adjusted ajax request to be just headers added a do while loop to fix duplicates still need to find out how to replace images also need to figure out how to track each image and store votes

--- vote_tracker1/kitten.js
+++ vote_tracker1/kitten.js
@@ -1,73 +1,51 @@
 window.onload = function () {
 
-  //possibly use for no repeat
-  // function someName(one, two, max) {
-  //   var newNum;
-  //   do {
-  //     newNum = Math.floor(Math.random() * max);
-  //   } while(newNum == one || newNum == two)
-  //   return newNum;
-  // }
+  var votes = 0;
+  var  Game = function() {
+    var trackCat1, trackCat2
+    var catPicsArray = []; //will contain the image link
 
-  // var  Game = function() {
-  var trackCat1, trackCat2;
+
+    var makeCats = function (image1) {
+       //loops through and selects each image link
+      for (var i = 0; i < image1.length; i++) {
+        catPicsArray.push(image1[i].link);
+      };
+      //gives random link to cat picture
+      trackCat1 = '<img src="' + catPicsArray[Math.floor(Math.random() * catPicsArray.length)] + '">';
+      do {
+        trackCat2 = '<img src="' + catPicsArray[Math.floor(Math.random() * catPicsArray.length)] + '">';
+      } while(trackCat1 == trackCat2) {
+
+      };
+
+      $(trackCat1).prependTo('#cat-pic');
+      $(trackCat2).prependTo('#cat-pic2');
+    }
+
     $.ajax({
       url: 'https://api.imgur.com/3/album/CTPOV.json',
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader('Authorization', 'Client-ID 11c372c0f3dfff2');
+      headers: {
+        'Authorization': 'Client-ID 11c372c0f3dfff2'
       }
     })
     .done( function(pics) {
-      makeCats(pics);
-      images = pics.data.images;
-      imagesLength = pics.data.images_count;
-      initialize();
-      user.displayPhotos();
+      images = pics.data.images; //images is now an array with each picture inside
+      makeCats(images);
 
     }).fail( function() { //Shows message if not sucessful
-      $('.grid_6').html('Can not load picture at the moment. Meow!')
+      $('.grid_6').html('Can not load picture right Meow!')
     });
-  // }
+  }
+  Game(); //so pictures show up
+
+//gives random number  for cat tracking
   var Tracker = function() {
     this.randPic = function() {
       return Math.floor(Math.random() * 14)
     }
-
-
   }
 
-  Tracker.prototype.pleaseMake = function() {
-    this.trackCat2 = this.randPic();
-    while (this.trackCat1 == this.trackCat2) {
-      this.trackCat2 = this.randPic();
-    }
-  };
-
-  var makeCats = function (image1) {
-    var catPicsArray = []; //loops through and selects each image
-      for (var i = 0; i < 14; i++) {
-        catPicsArray.push(image1.data.images[i].link);
-      };
-
-      var ChooseCat = function () {
-
-        return $('<img src="' + catPicsArray[Math.floor(Math.random() * catPicsArray.length)] + '">')
-      };
-
-      var $option1 = new ChooseCat();
-      var $option2 = new ChooseCat();
-      $option1.prependTo('#cat-pic');
-      $option2.prependTo('#cat-pic2');
-  }
-
-  var makeRandomCat = function() {
-
-  }
-
-  makeCats.prototype.ChooseCat = function() {
-
-
-  };
 
   //for tracking vote totals
   $('section.grid_6').on('click', function(){
@@ -78,6 +56,7 @@ window.onload = function () {
 
   $('.vote-again').on('click', function(e) { //click on vote again button
       e.preventDefault();//avoids refresh after clicking button
-      // Game();//starts function for a game again
+      Game();//starts function for a game again
+
     });
 }
